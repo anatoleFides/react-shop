ccb677dac0ecb05ce1c6ebd5b0074cea407f5c9d
Anatolii Bedilo <fides2013ml@gmail.com>
2022-06-07 21:30:06 +0300

    added admin page

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,14 +1,18 @@
 import React, { useContext, useState } from 'react'
 import Modal from "react-bootstrap/Modal"
-import { Form, Button, Dropdown } from "react-bootstrap"
+import { Form, Button, Dropdown, Row, Col } from "react-bootstrap"
 import { Context } from "../../index"
 
 const CreateDevice = ({ show, onHide }) => {
   const { device } = useContext(Context)
-  const { info, setInfo } = useState([])
+  const [info, setInfo] = useState([])
 
   const addInfo = () => {
     setInfo([...info, { title: '', description: '', number: Date.now() }])
+  }
+
+  const removeInfo = (number) => {
+    setInfo(info.filter(i => i.number !== number))
   }
 
   return (
@@ -23,42 +27,66 @@ const CreateDevice = ({ show, onHide }) => {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Dropdown>
-          <Dropdown.Toggle>Выберите тип</Dropdown.Toggle>
-          <Dropdown.Menu>
-            {device.types.map(type =>
-              <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
-            )}
-          </Dropdown.Menu>
-        </Dropdown>
-        <Dropdown className='mt-3'>
-          <Dropdown.Toggle>Выберите брэнд</Dropdown.Toggle>
-          <Dropdown.Menu>
-            {device.brands.map(brand =>
-              <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
-            )}
-          </Dropdown.Menu>
-        </Dropdown>
-        <Form.Control
-          className='mt-3'
-          placeholder='Добавить название устройства'
-        />
-        <Form.Control
-          className='mt-3'
-          placeholder='Добавить стоимость устройства'
-          type='number'
-        />
-        <Form.Control
-          className='mt-3'
-          type='file'
-        />
-        <Button
-          className='mt-3'
-          variant='outline-dark'
-          onClick={addInfo}
-        >
-          Добавить новое свойство
-        </Button>
+        <Form>
+          <Dropdown>
+            <Dropdown.Toggle>Выберите тип</Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.types.map(type =>
+                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Dropdown className='mt-3'>
+            <Dropdown.Toggle>Выберите брэнд</Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.brands.map(brand =>
+                <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Form.Control
+            className='mt-3'
+            placeholder='Добавить название устройства'
+          />
+          <Form.Control
+            className='mt-3'
+            placeholder='Добавить стоимость устройства'
+            type='number'
+          />
+          <Form.Control
+            className='mt-3'
+            type='file'
+          />
+          <hr />
+          <Button
+            variant={"outline-dark"}
+            onClick={addInfo}
+          >
+            Добавить новое свойство
+          </Button>
+          {info.map(i =>
+            <Row className="mt-4" key={i.number}>
+              <Col md={4}>
+                <Form.Control
+                  placeholder="Введите название свойства"
+                />
+              </Col>
+              <Col md={4}>
+                <Form.Control
+                  placeholder="Введите описание свойства"
+                />
+              </Col>
+              <Col md={4}>
+                <Button
+                  onClick={() => removeInfo(i.number)}
+                  variant={"outline-danger"}
+                >
+                  Удалить
+                </Button>
+              </Col>
+            </Row>
+          )}
+        </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
